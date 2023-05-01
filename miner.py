4bb697c1981cc8e4f568a1ea4d778fb3adaf0fa1
João Marcelo Dantas <jmgrd98@gmail.com>
2023-05-01 07:55:45 -0300

    Update miner.py

--- miner.py
+++ miner.py
@@ -13,3 +13,14 @@ def mine_btc(block_number, transactions, previous_hash, zero_qty):
             nonce += 1
             text = str(block_number) + transactions + previous_hash + str(nonce)
             new_hash = apply_sha256(text)
+        nonce += 1
+
+if __name__ == "__main__":
+    start = time.time()
+    mine_btc(5, "test transactions", "test previous hash", 4)
+    end = time.time()
+    print(f"Time taken: {end - start}")
+
+
+
+
